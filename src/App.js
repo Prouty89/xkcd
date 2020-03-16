eb5ba991338813fe9ff826c39f28295062c60ec0
Blake <bpro0821@gmail.com>
2020-03-16 06:37:01 -0500

    project complete, no styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+
 import './App.css';
 
+
+
 function App() {
+
+const [ comic, setComic ] = useState(null);
+const [ loading, setLoading ] = useState(true);
+const [ comicNum, setComicNum ] = useState(null);
+const [ error, setError ] = useState(null);
+const [ latest, setLatest ] = useState(null);
+
+const fetchComic = (number) => {
+  setLoading(true)
+  axios.get(`https://cors-anywhere.herokuapp.com/http://xkcd.com/${number}/info.0.json`)
+  .then(res => setComic(res.data))
+  .catch(err=>setError('error', err))
+  .finally(()=> setLoading(false))
+};
+
+const randomComicNum = () => {
+  const num = Math.floor(Math.random() * (latest -1) + 1);
+  return num;
+}
+
+const fetchLatestComic = () => {
+  setLoading(true);
+    axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json')
+    .then(res => {
+      setComic(res.data)
+      setLatest(res.data.num)
+      setLoading(false)
+    }) 
+    .catch(err => {
+      setError(err)
+      setLoading(false)
+    })
+}
+
+  useEffect(() => {
+    if(comicNum === null) {
+      fetchLatestComic()
+    } else {
+      fetchComic(comicNum);
+    }
+  }, [comicNum]);
+
+  
+
+  if(loading) {
+    return <div>
+      Loading...
+    </div>
+  };
+
+  if(error || !comic) {
+    return <div>
+      ERROR
+    </div>
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        disabled={comic.num === 1}
+        onClick={()=> setComicNum(1)}
+      >
+        First
+      </button>
+      <button 
+        disabled={comic.num <= 1}
+        onClick={()=> setComicNum(comic.num - 1)}
+      >
+        Previous
+      </button>
+      <button 
+      onClick={()=> setComicNum(randomComicNum())}>
+        Random
+      </button>
+      <button
+        disabled={comic.num === latest } 
+        onClick={()=> setComicNum(comic.num + 1)}
+      >
+        Next
+      </button>
+      <button
+        disabled={comic.num === latest }
+        onClick={()=> setComicNum(null)}
+      >
+        Latest
+      </button>
+      <div>{comic.title}
+      <img src={comic.img} alt="comic-img" />
+      </div>
+      
+      
     </div>
   );
 }
 
 export default App;
+
+
+
+
---
+++ src/components/IgnoreThis.js
@@ -0,0 +1,40 @@
+
+
+// const Person = ({ name, age }) => {
+//   console.log('Person props', name, age);
+ 
+//   return (
+//     <div>
+//       1st Person name: {name} age: {age}
+//     </div>
+//   )
+// }
+
+
+// function processPerson(person) {
+//   person.age,
+//   person.name
+// }
+
+// processPerson({ age: '26', name: 'Blake'});
+
+// const arr = [ 1, 2];
+
+// const first = [0]
+// const second = [1] verbose
+
+//Use destructuring
+
+// const [ first, second ] = [1, 2]; // Destructuring for arrays *Position matters
+
+// Destructure objects
+
+// const { name, age } = { name: 'Henry' , age: 26 } //Position is not important, the NAME is. Key - Value
+
+// // Now processPerson using destructuring
+
+// function processPerson({name, age, job, hairColor, location}) {
+ 
+// }
+
+// processPerson({ name: 'Blake', age: 30, job: 'Software Developer', hairColor: 'Brown', location: 'Sioux Falls'})
